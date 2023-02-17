89c44decd0a03142a661b485766afb05158bcf39
Guy <guy@guys-mbp.mynet>
2023-02-17 15:28:46 +0200

    cleanning & update the tesat

--- libs/jest_environment_testack/index.js
+++ libs/jest_environment_testack/index.js
@@ -3,82 +3,30 @@
 // import Testack from 'testackjs';
 
 // const TestEnvConfig = require('testack-interfaces');
-const NodeEnvironment = require('jest-environment-node');
+const NodeEnvironment = require('jest-environment-node').TestEnvironment;
 const Testack = require('testackjs').default;
 
 
-  var opts = undefined;
-  var global = undefined;
 
-  class TestackEnvironment extends NodeEnvironment {
-	// const opts = indefined;
-	// global: any;
-  // actions ;
+class TestackEnvironment extends NodeEnvironment {
+  opts = undefined;
   actions = {};
-  // constructor(config: Config.ProjectConfig, options: EnvironmentContext = {}) {
 
-  constructor(config={}, context={}) {
-    // config = config || {};
-    // if(!("testEnvironmentOptions" in config))
-    //   config["testEnvironmentOptions"] = {};
-
-
-    // config = config || {};
-    // config["testEnvironmentOptions"] = config.testEnvironmentOptions || {};
-    super(config,context);
-
+  constructor(config, context) {
+    super(config, context);
     this.opts = config.testEnvironmentOptions || {};
     this.opts.actions = this.opts.actions || [];
 
     // group actions by action event
-
-    this.opts.actions.map((action) => {
-      if (!this.actions[action.event]) {
-        this.actions[action.event] = [];
-      }
-      this.actions[action.event].push({provider: action.provider, method: action.method});
-    });
-
-    // this.opts.events.test_start = this.opts.events.test_start || [];
-    // this.opts.events.test_done = this.opts.events.test_done || [];
-    // this.opts.events.setup = this.opts.events.setup || [];
-    // this.opts.events.teardown = this.opts.events.teardown || [];
-    // this.opts.events.add_hook = this.opts.events.add_hook || [];
-    // this.opts.events.start_describe_definition = this.opts.events.start_describe_definition || [];
-    // this.opts.events.add_test = this.opts.events.add_test || [];
-    // this.opts.events.finish_describe_definition = this.opts.events.finish_describe_definition || [];
-    // this.opts.events.run_start = this.opts.events.run_start || [];
-    // this.opts.events.run_describe_start = this.opts.events.run_describe_start || [];
-    // this.opts.events.hook_start = this.opts.events.hook_start || [];
-    // this.opts.events.hook_success = this.opts.events.hook_success || [];
-    // this.opts.events.test_fn_start = this.opts.events.test_fn_start || [];
-    // this.opts.events.test_fn_success = this.opts.events.test_fn_success || [];    
-    // this.opts.events.run_describe_finish = this.opts.events.run_describe_finish || [];
-    // this.opts.events.run_finish = this.opts.events.run_finish || [];
-        
+    if(this.opts?.actions)
+      this.opts.actions.map((action) => {
+        if (!this.actions[action.event]) {
+          this.actions[action.event] = [];
+        }
+        this.actions[action.event].push({provider: action.provider, method: action.method});
+      });
     this.global.testack = new Testack(this.opts);
   }
-
-
-
-  // async setup() {
-  //   await this.doActions("beforeAll")
-  //   // this.opts.autoStartSession = this.opts.autoStartSession || typeof this.opts.autoStartSession == 'undefined';
-
-  //   // if (typeof this.opts.setup == 'function') {
-  //   //   await this.opts.setup.call(this.global, this.global.Testack);
-  //   // }
-  //   await super.setup();
-  // }
-
-  // async teardown() {
-  //   await this.doActions("afterAll") 
-  //   await super.teardown();
-
-  //   // if (typeof this.opts.teardown == 'function') {
-  //   //   await this.opts.teardown.call(this.global, this.global.Testack);
-  //   // }
-  // }
 
   async handleTestEvent(event, state) {    
   if( !event?.name) return;
---
+++ libs/jest_environment_testack/tests/env.test.js
@@ -0,0 +1,10 @@
+/**
+* @jest-environment ./
+*/
+
+describe('TestackEnvironment Unit Tests', function() {
+    it('create and verify the instance', function() {
+        expect(global.testack.constructor.name).toBe("Testack")
+    });  
+});
+  
