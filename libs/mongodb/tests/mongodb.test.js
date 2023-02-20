de0751d439073eb665cc6dd9735f702ef3ffc16e
megetron <geliyahu@gmail.com>
2023-02-20 11:11:30 +0200

    In memory mongodb (#6)
    adding support for inMemory mongodb

--- libs/mongodb/tests/mongodb.test.js
+++ libs/mongodb/tests/mongodb.test.js
@@ -1,63 +1,59 @@
-const MongoDB = require('../').MongoDB;
-const { MongoMemoryServer } = require('mongodb-memory-server')
+const {MongoDB} = require('../');
 
 var mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
-mongod= undefined
-mongod_uri = undefined
 mongodb_params = undefined
-Dog = undefined
+Dog = mongoose.model('Dog', new mongoose.Schema({ name: String, breed: String, age:Number }));
+
 describe('TestackEnvironment Unit Tests', function() {
   beforeAll(async () => {
-    mongod = await MongoMemoryServer.create();
-    mongod_uri = mongod.getUri();
-    MONGO_URI = mongod_uri.slice(0, mongod_uri.lastIndexOf('/')) + "/db";    
-
-    await mongoose.connect(`${MONGO_URI}`,{useNewUrlParser: true, useUnifiedTopology: true});
-    Dog = mongoose.model('Dog', new mongoose.Schema({ name: String, breed: String, age:Number }));
 
     mongodb_params = {
       provider: "MongoDB",
       username:"username",
-      password: "password",        
-      database: mongod.instanceInfo.dbName,
-      port: mongod.instanceInfo.port,
-      host: mongod.instanceInfo.ip
+      password: "password",
+      inMemory:true
     }
+  });
 
+  it('should create a MongoDB with inMemory database', async function() {
+    instance = await new MongoDB.create(mongodb_params)
+    expect(instance.host).toBe('127.0.0.1')
+    await instance.destroy();
   });
-  
-  afterAll(async () => {
-    await mongoose.connection.close()
-    await mongod.stop();
+
+  it('should create a MongoDB with real database', async function() {
+    const { inMemory, ...mongodb_params_real_db } = mongodb_params;
+
+    instance = await new MongoDB.create(mongodb_params_real_db)
+    expect(instance.port).toBe(27017)
+    await instance.destroy();
   });
+
+  it('should reset the database',  async function() {
+    instance = await new MongoDB.create(mongodb_params);
+    mongoose.connect(`${instance.DATABASE_URL}`,{useNewUrlParser: true, useUnifiedTopology: true});
     
 
-  it('should create a MongoDB provider with the params', function() {
-    instance = new MongoDB(mongodb_params)
-    expect(instance).toEqual(
-      expect.objectContaining(mongodb_params)
-    );
-  });
-
-  it('should reset the database',  function(done) {
     var dog1 = new Dog({ name: 'Snoop Doggy Dog'});
-    
-    dog1.save(async function (err, user1) {
-      if (err) return console.error(err);
-      await expect(Dog.count()).resolves.toBe(1);      
-      
-      instance = new MongoDB(mongodb_params)
-      await instance.reset();
-      await expect(Dog.count()).resolves.toBe(0);
-      done();
-    });
+
+    await dog1.save();
+    await expect(Dog.count()).resolves.toBe(1);
+    await instance.reset();
+    await expect(Dog.count()).resolves.toBe(0);
+    await instance.destroy();
+    await mongoose.connection.close()
+
   });
 
   it('should seed the database',  async function() {
-    instance = new MongoDB(mongodb_params);
+    instance = await new MongoDB.create(mongodb_params);
+    mongoose.connect(`${instance.DATABASE_URL}`,{useNewUrlParser: true, useUnifiedTopology: true});    
+    
     await instance.seed();
     await expect(Dog.count()).resolves.toBe(3);
+    await instance.destroy();
+    await mongoose.connection.close()
   });
 });
