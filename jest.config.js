e393b199ff28b9c23de40e8a2970ce1308e8be79
Guy <guy@guys-mbp.mynet>
2023-03-05 16:08:07 +0200

    fixing the ci

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
   testMatch: [
     '<rootDir>/libs/testack/tests/*.test.js',
     '<rootDir>/libs/mongodb/tests/*.test.js',
-    '<rootDir>/libs/jest_environment_testack/tests/*.test.js',
-    '<rootDir>/libs/create-testack/src/templates/javascript/tests/*.test.js'
+    '<rootDir>/libs/jest_environment_testack/tests/*.test.js'
+    // '<rootDir>/libs/create-testack/src/templates/javascript/tests/*.test.js'
   ],
 
   projects: [
@@ -21,12 +21,6 @@ module.exports = {
     {    
       displayName: 'testack',
       testMatch: ['<rootDir>/libs/testack/tests/*.test.js'],
-    },
-    {
-
-      // preset: 'ts-jest',
-      displayName: 'create-testack',
-      testMatch: ['<rootDir>/libs/create-testack/src/templates/javascript/tests/*.test.js'],
     }
   ],
 };
